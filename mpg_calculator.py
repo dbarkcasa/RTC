dfab78648558170a208b2d40a4623d1e2c61b990
Darryl Barkhouse <61213073+dbarkcasa@users.noreply.github.com>
2024-07-10 21:26:08 -0700

    Add files via upload
    This is simple script to demonstrate my understanding on Python variables and Print statements that we learned in lecture

---
+++ mpg_calculator.py
@@ -0,0 +1,14 @@
+'''
+This is simple script to demonstrate my understanding on
+Python vairables, Print statements that we learned in lecture
+'''
+
+# Get input from the user
+miles_driven = float(input("Enter the number of miles driven: "))
+gallons_used = float(input("Enter the number of gallons used: "))
+
+# Calculate miles per gallon
+mpg = (float)(miles_driven / gallons_used)
+
+# Display the result
+print(mpg)
