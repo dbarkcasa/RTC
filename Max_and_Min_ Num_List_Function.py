2f48fcdb7adf1153963857f7914f3e50b1e7c6fe
Darryl Barkhouse <61213073+dbarkcasa@users.noreply.github.com>
2024-08-04 14:57:08 -0700

    Add files via upload

---
+++ Max_and_Min_ Num_List_Function.py
@@ -0,0 +1,14 @@
+'''
+This program uses the built-in max() and min() functions to find the highest and lowest values in listA.
+'''
+
+# list
+listA = [23, 12, 45, 22, 11, 78, 13, 90, 45, 88, 10, -5, -1]
+
+# Built-in functions used on list
+max_num = max(listA)
+min_num = min(listA)
+
+# Print statements for results
+print("Maximum number:", max_num)
+print("Minimum number:", min_num)
